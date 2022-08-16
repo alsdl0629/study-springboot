9009fc1ee9bfe06b6c667898413181c4638b90d4
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-08-16 23:18:04 +0900

    ADD::User

---
+++ login/src/main/java/com/example/login/damain/user/entity/User.java
@@ -0,0 +1,39 @@
+package com.example.login.damain.user.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Authority authority;
+
+    @Builder
+    public User(String email, String password, String name, Authority authority) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.authority = authority;
+    }
+
+}
