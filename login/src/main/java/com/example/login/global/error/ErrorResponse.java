eed8196d2c278127c1508cf46e091d2d492c35c3
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-08-18 23:43:53 +0900

    ADD::convertObjectToJson

--- login/src/main/java/com/example/login/global/error/ErrorResponse.java
+++ login/src/main/java/com/example/login/global/error/ErrorResponse.java
@@ -1,5 +1,7 @@
 package com.example.login.global.error;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.*;
 
 @Getter
@@ -9,5 +11,8 @@ public class ErrorResponse {
     private final int status;
 
     private final String message;
-    
+
+    public String convertObjectToJson(Object object) throws JsonProcessingException {
+        return new ObjectMapper().writeValueAsString(object);
+    }
 }
