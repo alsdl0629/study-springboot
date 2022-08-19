5d4623f1e383fd90b8cb052c356b7daa99add887
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-08-19 23:37:16 +0900

    ADD::RefreshToken

---
+++ login/src/main/java/com/example/login/damain/refreshtoken/RefreshToken.java
@@ -0,0 +1,34 @@
+package com.example.login.damain.refreshtoken;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RedisHash
+public class RefreshToken {
+
+    @Id
+    private String userEmail;
+
+    @Indexed
+    private String refreshToken;
+
+    @TimeToLive
+    private Long ttl;
+
+    @Builder
+    public RefreshToken(String userEmail, String refreshToken, Long ttl) {
+        this.userEmail = userEmail;
+        this.refreshToken = refreshToken;
+        this.ttl = ttl;
+    }
+
+}
