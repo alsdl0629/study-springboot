2835294ed414464f8dc85b71243b937aa507eccf
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-08-19 23:52:50 +0900

    ADD::TokenResponse

---
+++ login/src/main/java/com/example/login/damain/user/api/dto/response/TokenResponse.java
@@ -0,0 +1,14 @@
+package com.example.login.damain.user.api.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TokenResponse {
+
+    private final String accessToken;
+
+    private final String refreshToken;
+
+}
