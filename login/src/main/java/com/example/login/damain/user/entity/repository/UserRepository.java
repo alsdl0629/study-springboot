fe2f4a21e386d550063b236f16cc17d9c2a532f0
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-08-25 19:12:39 +0900

    UPDATE::UserRepository

--- login/src/main/java/com/example/login/damain/user/entity/repository/UserRepository.java
+++ login/src/main/java/com/example/login/damain/user/entity/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.example.login.damain.user.entity.repository;
 import com.example.login.damain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
