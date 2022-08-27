4b21f4e8a861a28c03673a16031bfc706e5cfe17
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-08-28 02:30:33 +0900

    UPDATE::User

--- login/src/main/java/com/example/login/damain/user/domain/User.java
+++ login/src/main/java/com/example/login/damain/user/domain/User.java
@@ -1,8 +1,13 @@
 package com.example.login.damain.user.domain;
 
-import lombok.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -13,15 +18,20 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(nullable = false, unique = true)
+    @NotNull
+    @Size(max = 36)
+    @Column(unique = true)
     private String email;
 
-    @Column(nullable = false)
+    @NotNull
+    @Size(max = 15)
     private String password;
 
-    @Column(nullable = false)
+    @NotNull
+    @Size(max = 10)
     private String name;
 
+    @NotNull
     @Enumerated(EnumType.STRING)
     private Authority authority;
 
