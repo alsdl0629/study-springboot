5c5d1d0827190cb5d45a66a399a7fefc6ac8c36e
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-08-27 18:55:10 +0900

    UPDATE::UserSignUpService

--- login/src/main/java/com/example/login/damain/user/presentation/UserController.java
+++ login/src/main/java/com/example/login/damain/user/presentation/UserController.java
@@ -45,5 +45,5 @@ public class UserController {
     public TokenResponse login(@RequestBody @Valid LoginRequest request) {
         return userSignInService.execute(request);
     }
-    
+
 }
--- login/src/main/java/com/example/login/damain/user/service/UserSignUpService.java
+++ login/src/main/java/com/example/login/damain/user/service/UserSignUpService.java
@@ -3,8 +3,12 @@ package com.example.login.damain.user.service;
 import com.example.login.damain.user.entity.Authority;
 import com.example.login.damain.user.entity.User;
 import com.example.login.damain.user.entity.repository.UserRepository;
+import com.example.login.damain.user.exception.UnVerifiedAuthCodeException;
+import com.example.login.damain.user.facade.AuthCodeFacade;
 import com.example.login.damain.user.facade.UserFacade;
 import com.example.login.damain.user.presentation.dto.request.SignupRequest;
+import com.example.login.damain.user.presentation.dto.response.TokenResponse;
+import com.example.login.global.security.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -15,20 +19,32 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserSignUpService {
 
     private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
     private final PasswordEncoder passwordEncoder;
     private final UserFacade userFacade;
+    private final AuthCodeFacade authCodeFacade;
 
     @Transactional
-    public void execute(SignupRequest request) {
+    public TokenResponse execute(SignupRequest request) {
 
-        userFacade.isAlreadyExists(request.getEmail());
+        String email = request.getEmail();
+        String password = passwordEncoder.encode(request.getPassword());
+        String name = request.getName();
+
+        userFacade.isAlreadyExists(email);
+
+        if(!authCodeFacade.isVerify(email)) {
+            throw UnVerifiedAuthCodeException.EXCEPTION;
+        }
 
         userRepository.save(User.builder()
-                .email(request.getEmail())
-                .password(passwordEncoder.encode(request.getPassword()))
-                .name(request.getName())
+                .email(email)
+                .password(password)
+                .name(name)
                 .authority(Authority.ROLE_USER)
                 .build());
+
+        return jwtTokenProvider.generateToken(email);
     }
 
 }
