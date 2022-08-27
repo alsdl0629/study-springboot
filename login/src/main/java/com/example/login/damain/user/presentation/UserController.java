664949d8098c9427cd3a05d5f0512a6a50ededc6
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-08-27 18:50:02 +0900

    ADD::인증 확인

--- login/src/main/java/com/example/login/damain/user/presentation/UserController.java
+++ login/src/main/java/com/example/login/damain/user/presentation/UserController.java
@@ -3,10 +3,12 @@ package com.example.login.damain.user.presentation;
 import com.example.login.damain.user.presentation.dto.request.EmailRequest;
 import com.example.login.damain.user.presentation.dto.request.LoginRequest;
 import com.example.login.damain.user.presentation.dto.request.SignupRequest;
+import com.example.login.damain.user.presentation.dto.request.VerifyAuthCodeRequest;
 import com.example.login.damain.user.presentation.dto.response.TokenResponse;
 import com.example.login.damain.user.service.AuthCodeService;
 import com.example.login.damain.user.service.UserSignInService;
 import com.example.login.damain.user.service.UserSignUpService;
+import com.example.login.damain.user.service.VerifyAuthCodeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -21,21 +23,27 @@ public class UserController {
     private final UserSignUpService userSignUpService;
     private final UserSignInService userSignInService;
     private final AuthCodeService authCodeService;
-
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping
-    public void Signup(@RequestBody @Valid SignupRequest request) {
-        userSignUpService.execute(request);
-    }
+    private final VerifyAuthCodeService verifyAuthCodeService;
 
     @PostMapping("/email")
     public void sendAuthCode(@RequestBody @Valid EmailRequest request) {
         authCodeService.execute(request);
     }
 
+    @PutMapping("/email")
+    public void verifyEmail(@RequestBody @Valid VerifyAuthCodeRequest request) {
+        verifyAuthCodeService.execute(request);
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public TokenResponse Signup(@RequestBody @Valid SignupRequest request) {
+        return userSignUpService.execute(request);
+    }
+
     @PostMapping("/auth")
     public TokenResponse login(@RequestBody @Valid LoginRequest request) {
         return userSignInService.execute(request);
     }
-
+    
 }
---
+++ login/src/main/java/com/example/login/damain/user/service/VerifyAuthCodeService.java
@@ -0,0 +1,31 @@
+package com.example.login.damain.user.service;
+
+import com.example.login.damain.user.entity.AuthCode;
+import com.example.login.damain.user.entity.repository.AuthCodeRepository;
+import com.example.login.damain.user.exception.InvalidAuthCodeException;
+import com.example.login.damain.user.facade.AuthCodeFacade;
+import com.example.login.damain.user.presentation.dto.request.VerifyAuthCodeRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class VerifyAuthCodeService {
+
+    private final AuthCodeRepository authCodeRepository;
+    private final AuthCodeFacade authCodeFacade;
+
+    @Transactional
+    public void execute(VerifyAuthCodeRequest request) {
+
+        AuthCode authCode = authCodeFacade.getAuthCodeById(request.getEmail());
+
+        if(authCode.isVerify() || !authCode.getCode().equals(request.getCode())) {
+            throw InvalidAuthCodeException.EXCEPTION;
+        }
+
+        authCode.changeVerify();
+    }
+
+}
