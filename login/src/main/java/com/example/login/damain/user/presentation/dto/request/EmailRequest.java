1f7cb9c015a8091187b38e28765adb744632e99a
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-08-27 16:39:01 +0900

    ADD::EmailRequest

---
+++ login/src/main/java/com/example/login/damain/user/presentation/dto/request/EmailRequest.java
@@ -0,0 +1,17 @@
+package com.example.login.damain.user.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@NoArgsConstructor
+public class EmailRequest {
+
+    @NotBlank(message = "phone_number는 Null, 공백, 띄어쓰기를 허용하지 않습니다.")
+    @Email(message = "email 형식이 올바르지 않습니다.")
+    private String email;
+
+}
