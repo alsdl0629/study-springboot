1285c7cfa165f76541d4d1c4c269ccfacc901622
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-08-28 13:39:34 +0900

    UPDATE::RedisConfig

--- login/src/main/java/com/example/login/global/redis/RedisConfig.java
+++ login/src/main/java/com/example/login/global/redis/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.example.login.global.config;
+package com.example.login.global.redis;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
