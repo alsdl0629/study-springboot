f61ac53850f47794a862a72371f97abf7a6f7878
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-08-28 19:42:06 +0900

    ADD::ErrorCode

---
+++ image-upload/src/main/java/com/example/imageupload/global/error/ErrorCode.java
@@ -0,0 +1,17 @@
+package com.example.imageupload.global.error;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    USER_ALREADY_EXISTS(404, "User Already Exists")
+    ;
+
+    private final int status;
+
+    private final String message;
+
+}
