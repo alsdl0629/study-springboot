ef810586e3a05063b9065fb1487089a2d122b89d
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-09-06 17:13:58 +0900

    Init Project

---
+++ search-function/src/test/java/com/example/searchfunction/SearchFunctionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.searchfunction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchFunctionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
