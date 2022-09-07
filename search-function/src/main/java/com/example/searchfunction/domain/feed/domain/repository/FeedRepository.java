5246bbfa44420f629de66f040ad6fc80363dfcc2
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-09-07 11:00:27 +0900

    UPDATE::FindByTitleContaining

--- search-function/src/main/java/com/example/searchfunction/domain/feed/domain/repository/FeedRepository.java
+++ search-function/src/main/java/com/example/searchfunction/domain/feed/domain/repository/FeedRepository.java
@@ -3,5 +3,8 @@ package com.example.searchfunction.domain.feed.domain.repository;
 import com.example.searchfunction.domain.feed.domain.Feed;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FeedRepository extends JpaRepository<Feed, Integer> {
+    List<Feed> findByTitleContaining(String keyword);
 }
