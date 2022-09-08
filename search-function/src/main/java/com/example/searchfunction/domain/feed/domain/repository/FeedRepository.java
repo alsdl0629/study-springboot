50ff1d330bc0ab3af5b497518cd86c161b7ffbec
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-09-08 12:24:58 +0900

    UPDATE::FeedRepository Pageable

--- search-function/src/main/java/com/example/searchfunction/domain/feed/domain/repository/FeedRepository.java
+++ search-function/src/main/java/com/example/searchfunction/domain/feed/domain/repository/FeedRepository.java
@@ -1,10 +1,10 @@
 package com.example.searchfunction.domain.feed.domain.repository;
 
 import com.example.searchfunction.domain.feed.domain.Feed;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface FeedRepository extends JpaRepository<Feed, Integer> {
-    List<Feed> findByTitleContaining(String keyword);
+    Page<Feed> findByTitleContaining(String keyword, Pageable pageable);
 }
