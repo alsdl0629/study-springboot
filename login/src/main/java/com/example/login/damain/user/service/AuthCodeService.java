bbbfff24ec389f0899b5b5a096156b16be8175ae
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-09-12 00:33:03 +0900

    UPDATE::AuthCodeService

--- login/src/main/java/com/example/login/damain/user/service/AuthCodeService.java
+++ login/src/main/java/com/example/login/damain/user/service/AuthCodeService.java
@@ -1,8 +1,7 @@
 package com.example.login.damain.user.service;
 
-import com.example.login.damain.user.domain.repository.UserRepository;
-import com.example.login.damain.user.exception.UserAlreadyExistsException;
 import com.example.login.damain.user.facade.AuthCodeFacade;
+import com.example.login.damain.user.facade.UserFacade;
 import com.example.login.damain.user.presentation.dto.request.EmailRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,8 +10,8 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Service
 public class AuthCodeService {
-
-    private final UserRepository userRepository;
+    
+    private final UserFacade userFacade;
     private final AuthCodeFacade authCodeFacade;
 
     @Transactional
@@ -20,9 +19,7 @@ public class AuthCodeService {
 
         String email = request.getEmail();
 
-        if(userRepository.existsByEmail(email)) {
-           throw UserAlreadyExistsException.EXCEPTION;
-        }
+        userFacade.isAlreadyExists(request.getEmail());
 
         authCodeFacade.sendEmail(email);
     }
