3dd19b6582267e1323b7308972ca133383016cd8
Kangmin <ehfehf0629@gmail.com>
2022-10-15 23:55:18 +0900

    Init Project

---
+++ socket-communication/src/main/java/com/example/socketcommunication/SocketCommunicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.socketcommunication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketCommunicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocketCommunicationApplication.class, args);
+    }
+
+}
