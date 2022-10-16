205628b983fdfd4004bf11005f43a013627e2525
Kangmin <ehfehf0629@gmail.com>
2022-10-16 23:22:02 +0900

    DELETE::여기에...

--- schedule-management/src/main/java/com/example/schedulemanagement/domain/notice/domain/repository/NoticeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.schedulemanagement.domain.notice.domain.repository;
-
-import com.example.schedulemanagement.domain.notice.domain.Notice;
-import org.springframework.data.repository.CrudRepository;
-
-public interface NoticeRepository extends CrudRepository<Notice, Integer> {
-}
