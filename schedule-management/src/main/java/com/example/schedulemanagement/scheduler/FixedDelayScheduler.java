52b3e5fe2ef7a61870129058213436ef89d71f42
Kangmin <ehfehf0629@gmail.com>
2022-10-30 18:56:35 +0900

    UPDATE::FixedDelayScheduler

--- schedule-management/src/main/java/com/example/schedulemanagement/scheduler/FixedDelayScheduler.java
+++ schedule-management/src/main/java/com/example/schedulemanagement/scheduler/FixedDelayScheduler.java
@@ -8,7 +8,7 @@ public class FixedDelayScheduler {
 
     @Scheduled(fixedDelay = 60 * 1000)
     public void fixedDelaySchedule() {
-        System.out.println("asdf");
+        System.out.println("Component 어노테이션 붙여야함, 반환타입은 void");
     }
 
 }
