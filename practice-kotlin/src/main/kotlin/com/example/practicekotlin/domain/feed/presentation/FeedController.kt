bc7ba7dc52d301192295efd45c550ad8e61eab06
Kangmin <ehfehf0629@gmail.com>
2022-12-31 23:27:07 +0900

    add: CreateFeedService

---
+++ practice-kotlin/src/main/kotlin/com/example/practicekotlin/domain/feed/presentation/FeedController.kt
@@ -0,0 +1,22 @@
+package com.example.practicekotlin.domain.feed.presentation
+
+import com.example.practicekotlin.domain.feed.presentation.dto.request.CreateFeedRequest
+import com.example.practicekotlin.domain.feed.service.CreateFeedService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/feeds")
+@RestController
+class FeedController(private val createFeedService: CreateFeedService) {
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    fun createFeed(@RequestBody request: CreateFeedRequest) {
+        createFeedService.execute(request)
+    }
+
+}
