e94aa97d84a891a3b2ae1979faa0e7adb1af42b7
Kangmin <ehfehf0629@gmail.com>
2023-01-04 22:58:17 +0900

    fix: FeedController

--- practice-kotlin/src/main/kotlin/com/example/practicekotlin/domain/feed/presentation/FeedController.kt
+++ practice-kotlin/src/main/kotlin/com/example/practicekotlin/domain/feed/presentation/FeedController.kt
@@ -1,18 +1,17 @@
 package com.example.practicekotlin.domain.feed.presentation
 
 import com.example.practicekotlin.domain.feed.presentation.dto.request.CreateFeedRequest
+import com.example.practicekotlin.domain.feed.presentation.dto.request.UpdateFeedRequest
 import com.example.practicekotlin.domain.feed.service.CreateFeedService
+import com.example.practicekotlin.domain.feed.service.UpdateFeedService
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RequestMapping("/feeds")
 @RestController
 class FeedController(
-    private val createFeedService: CreateFeedService
+    private val createFeedService: CreateFeedService,
+    private val updateFeedService: UpdateFeedService
 ) {
 
     @ResponseStatus(HttpStatus.CREATED)
@@ -20,4 +19,13 @@ class FeedController(
     fun createFeed(@RequestBody request: CreateFeedRequest) {
         createFeedService.execute(request)
     }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PatchMapping("/{feed-id}")
+    fun updateFeed(@PathVariable("feed-id") feedId: Long,
+                   @RequestBody request: UpdateFeedRequest
+    ) {
+        updateFeedService.execute(feedId, request)
+    }
+
 }
