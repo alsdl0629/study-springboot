45c82200a586a61f307a5066f60f1d24b84f6260
Kangmin <ehfehf0629@gmail.com>
2023-01-08 00:00:09 +0900

    setting

--- practice-kotlin/build.gradle.kts
+++ practice-kotlin/build.gradle.kts
@@ -27,6 +27,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("io.jsonwebtoken:jjwt:0.9.0")
 
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
