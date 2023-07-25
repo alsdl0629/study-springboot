a76bb29f9fd952f4b1eb5060d52f42e67bd02aab
Kangmin <ehfehf0629@gmail.com>
2023-07-25 16:40:35 +0900

    chore: async

--- event/src/main/kotlin/com/example/event/EntityEventHandler.kt
+++ event/src/main/kotlin/com/example/event/EntityEventHandler.kt
@@ -1,15 +1,18 @@
 package com.example.event
 
 import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 
 @Component
 class EntityEventHandler {
 
+    @Async
     @EventListener
     fun on(entityCreated: EntityCreated) {
         entityCreated.apply {
             println("$entityId 번 피드가 저장됨")
+            println(Thread.currentThread().id)
         }
     }
 }
